768f58ef262d7dfa022642aaa2faa6cdcd8bf792
dhawangayash <dhawan.gajendran@datavisor.com>
2017-08-21 22:10:48 -0700

    Update JettySinkServer.java

--- src/main/java/com/jetty/server/JettySinkServer.java
+++ src/main/java/com/jetty/server/JettySinkServer.java
@@ -1,16 +1,3 @@
-/**
- * Copyright (c) 2017, DATAVISOR, INC.
- * All rights reserved.
- * __________________
- * <p>
- * NOTICE: All information contained herein is, and remains the property
- * of DataVisor, Inc.  The intellectual and technical concepts contained
- * herein are proprietary to DataVisor, Inc. and may be covered by
- * U.S. and Foreign Patents, patents in process, and are protected by
- * trade secret or copyright law.  Dissemination of this information or
- * reproduction of this material is strictly forbidden unless prior
- * written permission is obtained from DataVisor, Inc.
- */
 package com.jetty.server;
 
 import io.prometheus.client.exporter.MetricsServlet;
